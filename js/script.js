25551786cbd957884b8df3a044ecf2772a1ff451
Adniloet Sosa <adni.sori@gmail.com>
2024-11-11 14:49:55 +0100

    Continuación de JS

--- js/script.js
+++ js/script.js
@@ -3,12 +3,19 @@ const nextButton = document.getElementById('next-page');
 const charactersList = document.getElementById('character-list');
 const charactersAPI = 'https://rickandmortyapi.com/api/character';
 
-function getCharacter() {
+window.addEventListener('DOMContentLoaded', () => {
     fetch(charactersAPI)
     .then((response) => {
         if(!response.ok) {
             throw new Error('La solicitud no ha sido exitosa')
         }
+        return response.json();
     })
-}
-
+    .then((data) => {
+        console.log(data.results);
+        for (let i = 0; i < data.results.length; i++) {
+            charactersList.textContent = data.results[i].name;
+        }
+        return charactersList;
+    })
+})
